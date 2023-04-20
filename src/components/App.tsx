fb65454c6a8d7459dc688c3108696fcb944f67a3
Ricardo Casares <ricardocasares@gmail.com>
2023-04-20 21:37:23 +0200

    cleanup business logic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,74 +3,13 @@ import { Preview } from "./Preview";
 import { Workbench } from "./Workbench";
 import { RegularExpression } from "./RegularExpression";
 import { help } from "../lib/help";
-import { escape } from "../lib/util";
 import { Layout } from "./Layout";
-
-export type TLineMatcher = {
-  text: string;
-  patterns: [string, string][];
-};
-
-export type TLineMatchers = Record<string, TLineMatcher>;
-
-export type TMatcherData = {
-  text: string;
-  pattern: string;
-};
+import { useRegexGen } from "../lib/hooks";
 
 export default function App() {
+  const core = useRegexGen();
   const [lines, setLines] = useState<string[]>(help);
-  const [lineMatchers, setLineMatchers] = useState<TLineMatchers>({});
-
-  const regex = Object.entries(lineMatchers)
-    .map(([_, { text, patterns }]) =>
-      // apply all the patterns to the line
-      patterns.reduce((acc, [a, b]) => acc.replace(escape(a), b), escape(text))
-    )
-    .join(`(\\n(?:\\s.*\\n)*\\s*)`);
-
-  /**
-   * Toggles a line to be selected
-   * @param num line number
-   * @param line line text
-   */
-  const toggleSelectedLine = (num: number, text: string) => {
-    switch (lineMatchers.hasOwnProperty(num)) {
-      case true:
-        // matcher already exists for this line so we remove it
-        const { [num]: _, ...rest } = lineMatchers;
-        return setLineMatchers({ ...rest });
-      case false:
-        // in this case we add a new matcher to this line number
-        return setLineMatchers({
-          ...lineMatchers,
-          [num]: { text, patterns: [] },
-        });
-    }
-  };
-
-  /**
-   * Toggles a pattern inside a line
-   * @param num line number
-   * @param matcher text to match along with an array of patterns
-   */
-  const toggleSelectedPattern = (num: number, matcher: TMatcherData) => {
-    const { patterns } = lineMatchers[num];
-    // extract the text and pattern
-    const { text, pattern } = matcher;
-    // check if the pattern is already selected
-    const exists = patterns.find(([x]) => x === text);
-    // either add or remove the pattern
-    const updated = exists
-      ? patterns.filter(([x]) => x !== text)
-      : [...patterns, [text, pattern]];
-
-    // update the state
-    setLineMatchers({
-      ...lineMatchers,
-      [num]: { ...lineMatchers[num], patterns: updated },
-    });
-  };
+  const regex = core.generate();
 
   const handleSelections: (
     idx: number,
@@ -85,11 +24,11 @@ export default function App() {
         // event is not coming from the color block
         if (!event.target?.dataset?.matcher) return;
         // @ts-expect-error
-        const { text, pattern }: TMatcherData = event?.target?.dataset;
-        return toggleSelectedPattern(idx, { text, pattern });
+        const { text, pattern } = event?.target?.dataset;
+        return core.toggleRegex(idx, [text, pattern]);
       // Double click
       case 2:
-        return toggleSelectedLine(idx, line);
+        return core.toggleLine(idx, line);
       default:
         return;
     }
@@ -97,7 +36,7 @@ export default function App() {
 
   useEffect(() => {
     const handlePaste = (event: ClipboardEvent) => {
-      setLineMatchers({});
+      core.reset();
       setLines(event.clipboardData?.getData("text").split("\n") ?? []);
     };
 
@@ -110,7 +49,7 @@ export default function App() {
       <Workbench
         lines={lines}
         onClick={handleSelections}
-        isSelected={(id) => lineMatchers.hasOwnProperty(id)}
+        isSelected={(id) => core.hasLine(id)}
       />
       <Preview text={lines.join("\n")} regex={regex} />
       <RegularExpression regex={regex} />
---
+++ src/lib/hooks.ts
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { escape } from "./util";
+
+export type Regex = [string, string];
+
+export type LineRegexes = {
+  text: string;
+  regexes: Regex[];
+};
+
+export function useRegexGen() {
+  const [lines, setLines] = useState<Record<string, LineRegexes>>({});
+
+  function reset() {
+    setLines({});
+  }
+
+  function hasLine(n: number) {
+    return !!lines[n];
+  }
+
+  function selectLine(n: number, text: string) {
+    setLines({ ...lines, [n]: { text, regexes: [] } });
+  }
+
+  function deselectLine(n: number) {
+    const { [n]: _, ...rest } = lines;
+    setLines({ ...rest });
+  }
+
+  function toggleLine(n: number, text: string) {
+    return hasLine(n) ? deselectLine(n) : selectLine(n, text);
+  }
+
+  function lineHasRegex(n: number, [text]: Regex) {
+    const { regexes = [] } = lines[n];
+    return !!regexes.find(([txt]) => txt === text);
+  }
+
+  function addRegex(n: number, regex: Regex) {
+    if (hasLine(n) && !lineHasRegex(n, regex)) {
+      const { text, regexes = [] } = lines[n];
+      setLines({ ...lines, [n]: { text, regexes: [...regexes, regex] } });
+    }
+  }
+
+  function removeRegex(n: number, regex: Regex) {
+    if (lineHasRegex(n, regex)) {
+      const { text, regexes: old } = lines[n];
+      const regexes = old.filter(([txt]) => txt === text);
+      setLines({ ...lines, [n]: { text, regexes } });
+    }
+  }
+
+  function toggleRegex(n: number, regex: Regex) {
+    return lineHasRegex(n, regex) ? removeRegex(n, regex) : addRegex(n, regex);
+  }
+
+  function generate() {
+    return Object.entries(lines)
+      .map(([_, { text, regexes }]) =>
+        // apply all the patterns to the line
+        regexes.reduce((acc, [a, b]) => acc.replace(escape(a), b), escape(text))
+      )
+      .join(`(\\n(?:\\s.*\\n)*\\s*)`);
+  }
+
+  return {
+    lines,
+    reset,
+    hasLine,
+    selectLine,
+    deselectLine,
+    toggleLine,
+    lineHasRegex,
+    addRegex,
+    removeRegex,
+    toggleRegex,
+    generate,
+  };
+}
+
