d332fe37266f252bc95d10da339cdc30060f5b25
Ricardo Casares <84963+ricardocasares@users.noreply.github.com>
2023-04-20 13:04:34 +0200

    update regex escaping (#1)

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,2 +1,6 @@
 export const wait: (n: number) => Promise<void> = (n) =>
   new Promise((r) => setTimeout(r, n));
+
+export const escape = (str: string) => {
+  return str.replace(/[.*+?^${}()/|[\]\\]/g, "\\$&");
+};
