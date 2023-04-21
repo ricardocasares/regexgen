402a2427846905003699da59a6189cbee6d67c1d
pcecot <80515130+pcecot@users.noreply.github.com>
2023-04-21 15:14:36 +0200

    add more regexes and updated help.ts (#2)
    * fixing conflicts on parser.tsx
    
    * updated some of the regexes and added test strings to help.ts file
    
    * forgot the add changes in parser regexes

--- src/lib/parser.tsx
+++ src/lib/parser.tsx
@@ -2,43 +2,56 @@ import reactStringReplace from "react-string-replace-recursively";
 import { Match } from "../components/Match";
 
 export const parser = reactStringReplace({
-  ratio: {
-    pattern: /(\d{1,3}\/\d{1,3})/,
-    ignore: ["ratio"],
-    matcherFn: (raw, processed, key) => (
-      <Match
-        title="Ratio"
-        key={key}
-        data-text={raw}
-        data-pattern="(\d{1,3}\/\d{1,3})"
-      >
-        {processed}
-      </Match>
-    ),
-  },
-  macAddress: {
-    pattern: /([0-9a-f]{4}[\\.][0-9a-f]{4}[\\.][0-9a-f]{4})/,
-    ignore: ["float", "digit", "word"],
-    matcherFn: (raw, processed, key) => (
-      <Match
-        key={key}
-        title="MAC address"
-        data-text={raw}
-        data-pattern="([0-9a-f]{4}[\\.][0-9a-f]{4}[\\.][0-9a-f]{4})"
-      >
-        {processed}
-      </Match>
-    ),
-  },
   interface: {
-    pattern: /(Ethernet|Serial|TokenRing\s?\d)/,
-    ignore: ["float", "digit", "word"],
+    pattern: /((?:\S*Ethernet|\S*GigE|Loopback|Tunnel|Serial|Port-channel)\d+(?:\/\d+)*(?:\.\d+)*)/g,
+    ignore: ["float", "word", "ratio"],
     matcherFn: (raw, processed, key) => (
       <Match
         key={key}
         title="Interface"
         data-text={raw}
-        data-pattern="(Ethernet\d|Serial\d|TokenRing\d)"
+        data-pattern="((?:\S*Ethernet|\S*GigE|Loopback|Tunnel|Serial|Port-channel)\d+(?:\/\d+)*(?:\.\d+)*)"
+      >
+        {processed}
+      </Match>
+    ),
+  },
+  ratio: {
+    pattern: /((?:\d+\/)+\d+)/g,
+    matcherFn: (raw, processed, key) => (
+      <Match
+        title="Ratio"
+        key={key}
+        data-text={raw}
+        data-pattern="((\d+\/)+\d+)"
+      >
+        {processed}
+      </Match>
+    ),
+  },
+  macAddress1: {
+    pattern: /((?:[a-fA-F\d]{4}\.){2}[a-fA-F\d]{4})/g,
+    ignore: ["float", "integer", "word"],
+    matcherFn: (raw, processed, key) => (
+      <Match
+        key={key}
+        title="MAC address (1)"
+        data-text={raw}
+        data-pattern="(([a-fA-F\d]{4}\.){2}[a-fA-F\d]{4})"
+      >
+        {processed}
+      </Match>
+    ),
+  },
+  macAddress2: {
+    pattern: /((?:[a-fA-F\d]{2}:){5}[a-fA-F\d]{2})/g,
+    ignore: ["float", "integer", "word"],
+    matcherFn: (raw, processed, key) => (
+      <Match
+        key={key}
+        title="MAC address (2)"
+        data-text={raw}
+        data-pattern="(([a-fA-F\d]{2}:){5}[a-fA-F\d]{2})"
       >
         {processed}
       </Match>
@@ -46,7 +59,7 @@ export const parser = reactStringReplace({
   },
   ipAddress: {
     pattern: /(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)/,
-    ignore: ["float", "digit", "word"],
+    ignore: ["float", "integer", "word"],
     matcherFn: (raw, processed, key) => (
       <Match
         title="IP Address"
@@ -59,8 +72,8 @@ export const parser = reactStringReplace({
     ),
   },
   time: {
-    pattern: /(\d{1,2}:\d{2}:\d{2})/,
-    ignore: ["digit", "word"],
+    pattern: /(\d{1,2}:\d{2}:\d{2})/g,
+    ignore: ["integer", "word"],
     matcherFn: (raw, processed, key) => (
       <Match
         title="Time"
@@ -73,8 +86,8 @@ export const parser = reactStringReplace({
     ),
   },
   float: {
-    pattern: /(\d{1,4}\.\d{1,4})/,
-    ignore: ["digit"],
+    pattern: /(\d{1,4}\.\d{1,4})/g,
+    ignore: ["integer"],
     matcherFn: (raw, processed, key) => (
       <Match
         title="Float"
@@ -86,8 +99,8 @@ export const parser = reactStringReplace({
       </Match>
     ),
   },
-  digit: {
-    pattern: /(\d)/,
+  integer: {
+    pattern: /(\d)/g,
     ignore: ["word"],
     matcherFn: (raw, processed, key) => (
       <Match title="Integer" key={key} data-text={raw} data-pattern="(\d)">
