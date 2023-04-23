49a9da14d9f5bed347fa0adb69619c41943f1a93
Ricardo Casares <ricardocasares@gmail.com>
2023-04-23 05:33:59 +0200

    copy gives you a random compliment

--- src/components/ReExp/ReExp.tsx
+++ src/components/ReExp/ReExp.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { Stack } from "../Stack";
 import css from "./reexp.module.css";
-import { wait } from "../../lib/util";
+import { wait, randomCompliment } from "../../lib/util";
 import { useCore } from "../../core";
 import { Button } from "../Button";
 
@@ -10,17 +9,17 @@ export const ReExp = () => {
   const [copied, setCopied] = useState(false);
   const help = "Your regex will appear here, then click to copy";
   const ready = Boolean(regex.length);
-  const displayText = copied ? "Copied!" : ready ? regex : help;
+  const displayText = copied ? randomCompliment() : ready ? regex : help;
 
   const onClick = () =>
     Promise.resolve(setCopied(true))
       .then(() => navigator.clipboard.writeText(regex).catch(console.log))
-      .then(() => wait(500))
+      .then(() => wait(900))
       .then(() => setCopied(false));
 
   return (
     <Button
-      disabled={!Boolean(regex.length)}
+      disabled={!Boolean(regex.length) || copied}
       className={css.regex}
       secondary
       onClick={onClick}
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -8,3 +8,18 @@ export function splitByLine(s: string) {
 export const escape = (str: string = "") => {
   return str.replace(/[.*+?^${}()/|[\]\\]/g, "\\$&");
 };
+
+export const pickOne = <T>(items: T[]) =>
+  items[Math.floor(Math.random() * items.length)];
+
+export const randomCompliment = () => pickOne(compliments);
+
+export const compliments = [
+  "Nice!",
+  "You got this!",
+  "Amazing!",
+  "Great job!",
+  "You're a smart cookie!",
+  "You have the best ideas!",
+  "You look great today :)",
+];
